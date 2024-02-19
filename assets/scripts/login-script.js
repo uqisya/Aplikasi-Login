cac24f59396c94ec0949b05c473b2b5c0636e8c1
uqi <88234520+uqisya@users.noreply.github.com>
2024-02-19 20:09:40 +0700

    Update login-script.js
    Mengganti comment agar sesuai dengan JSDoc

--- assets/scripts/login-script.js
+++ assets/scripts/login-script.js
@@ -1,25 +1,57 @@
-/*
- * Catatan:
- * Gunakan fungsi goToHome() untuk menampilkan halaman home
- * Gunakan fungsi goToLogin() untuk menampilkan halaman login
- * Gunakan fungsi showPopUp() untuk menampilkan pop up error
- */
-
+/** @module Login-Script */
+/** 
+* Membuat variabel loginFormElement untuk tampilan form.
+* @constant {HTMLElement}
+*/
 const loginFormElement = document.querySelector('#loginForm');
+
+/** 
+* Membuat variabel inputEmailElement untuk tampilan input email.
+* @constant {HTMLElement}
+*/
 const inputEmailElement = document.querySelector('#inputEmail');
+
+/** 
+* Membuat variabel inputPasswordElement untuk tampilan input password.
+* @constant {HTMLElement}
+*/
 const inputPasswordElement = document.querySelector('#inputPassword');
 
+/** 
+ * Membuat variabel expectedEmail untuk menyimpan informasi email sementara.
+ * @constant {string}
+ */
 const expectedEmail = 'admin@dicoding.com';
+
+/** 
+ * Membuat variabel expectedPassword untuk menyimpan informasi password sementara.
+ * @constant {string}
+ */
 const expectedPassword = 'superpassword';
 
+/* Comment:  Menambahkan aksi klik pada button */
 loginFormElement.addEventListener('submit', function(event) {
-  event.preventDefault();
+    event.preventDefault();
 
-  const email = inputEmailElement.value;
-  const password = inputPasswordElement.value;
-  if (email == expectedEmail && password == expectedPassword) {
-    goToHome();
-  } else {
-    showPopUp();
-  }
+    /** 
+     * Membuat variabel email untuk menyimpan nilai email yang didapatkan saat button ditekan.
+     * @constant {string}
+     */
+    const email = inputEmailElement.value;
+
+    /** 
+     * Membuat variabel password untuk menyimpan niali password yang didapatkan saat button ditekan.
+     * @constant {string}
+     */
+    const password = inputPasswordElement.value;
+
+    /* Comment: validasi nilai email dan password harus sama dengan nilai yang tersimpan */
+    if(email == expectedEmail && password == expectedPassword){
+      /* Comment: jika sesuai, maka arahkan ke halaman home */
+      goToHome();
+    }
+    else{
+      /* Comment: jika tidak sesuai, berikan pop up message kesalahan pada input */
+      showPopUp();
+    }
 });
